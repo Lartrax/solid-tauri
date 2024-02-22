ed2d006ef9940dfb95d552e621acf4e16e104363
Lars Erik Nordbø <lartrax909@gmail.com>
2024-02-22 22:35:24 +0100

    move menu to new header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import { For, createEffect, createSignal, type Component } from "solid-js";
 
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import { logger } from "./functions";
-import icon from "./assets/icon.ico";
 
 import { invoke } from "@tauri-apps/api";
-import { appWindow } from "@tauri-apps/api/window";
+import { Header } from "./components/header/header";
 
 
 const App: Component = () => {
   const [saveText, setSaveText] = createSignal("");
   const [search, setSearch] = createSignal("");
   const [filteredFoods, setFilteredFoods] = createSignal<string[]>([]);
-  const [isMaximized, setIsMaximized] = createSignal(false);
 
   const foods = [
     "apple",
@@ -63,94 +60,49 @@ const App: Component = () => {
 
   invoke<string>("save").then((res) => setSaveText(res));
 
-  const dragWindow = async () => {
-    await appWindow.startDragging();
-  };
-  const maximizeWindow = async () => {
-    if (await appWindow.isMaximized()) {
-      setIsMaximized(false);
-      await appWindow.unmaximize();
-    } else {
-      setIsMaximized(true);
-      await appWindow.maximize();
-    }
-
-  };
-  const minimizeWindow = async () => {
-    await appWindow.minimize();
-  };
-  const closeWindow = async () => {
-    await appWindow.close();
-  };
-
   return (
     <div class={styles.App}>
+      <Header />
+      <img src={logo} class={styles.logo} alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to {saveText()}.
+      </p>
+      <a
+        class={styles.link}
+        href="https://github.com/solidjs/solid"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Solid
+      </a>
       <div
         style={{
-          "z-index": 1,
-          height: "32px",
-          width: "100%",
-          position: "fixed",
-          top: 0,
-          "background-color": "rgba(255, 255, 255, 20%)",
-          display: "flex"
+          display: "flex",
+          "flex-direction": "column",
+          "margin-top": "1em",
         }}
       >
-        <div onPointerDown={dragWindow} style={{ display: "flex", "flex-grow": 1, "align-items": "center" }}>
-          <img src={icon} style={{ height: "26px", "margin-left": "4px" }} />
-        </div>
-        <div class={styles.button} onClick={minimizeWindow} style={{ "font-size": "0.7em" }}>
-          —
-        </div>
-        <div class={styles.button} onClick={maximizeWindow} style={{ "font-size": "1.1em" }}>
-          {isMaximized() ? "◱" : "◻"}
-        </div>
-        <div class={styles.buttonX} onClick={closeWindow} style={{ "font-size": "1em" }}>
-          ✕
-        </div>
-      </div>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to {saveText()}.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <input
+          style={{
+            "padding-left": "4px",
+            width: "50%",
+            "align-self": "center",
+          }}
+          onInput={(e) => setSearch(e.target.value)}
+          placeholder="Search..."
+        />
         <div
           style={{
             display: "flex",
-            "flex-direction": "column",
-            "margin-top": "1em",
+            "column-gap": "1em",
+            "flex-wrap": "wrap",
+            "align-self": "center",
+            width: "50%",
           }}
         >
-          <input
-            style={{
-              "padding-left": "4px",
-              width: "50%",
-              "align-self": "center",
-            }}
-            onInput={(e) => setSearch(e.target.value)}
-            placeholder="Search..."
-          />
-          <div
-            style={{
-              display: "flex",
-              "column-gap": "1em",
-              "flex-wrap": "wrap",
-              "align-self": "center",
-              width: "50%",
-            }}
-          >
-            <For each={filteredFoods()}>{(food) => <span>{food}</span>}</For>
-          </div>
+          <For each={filteredFoods()}>{(food) => <span>{food}</span>}</For>
         </div>
-      </header>
+      </div>
     </div>
   );
 };
