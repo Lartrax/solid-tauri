8f8ad2d0236c230c8aa5a2389d910fe7a452424a
Lars Erik Nordbø <89910638+Lartrax@users.noreply.github.com>
2024-03-01 14:30:22 +0100

    Add word-distance breakdown (#3)

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -14,13 +14,13 @@ const IconButton: Component<buttonProps> = ({ icon, onClick, style }) => {
       text={icon}
       onClick={onClick}
       style={{
-        ...style,
         height: "auto",
         "font-size": "1.5em",
         "font-weight": "350",
         "line-height": "1.5em",
         "padding-left": "0.5em",
         "padding-right": " 0.5em",
+        ...style,
       }}
     />
   );
